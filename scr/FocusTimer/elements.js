c3f88679b6d269010b8987860a06c1d1110382c2
LucasEduChaves <lucasedchaves090@gmail.com>
2023-08-22 12:21:03 -0300

    first commit

---
+++ scr/FocusTimer/elements.js
@@ -0,0 +1,8 @@
+export const controls = document.getElementById
+('controls')
+
+export const minutes = document.getElementById
+('minutes')
+
+export const seconds = document.getElementById
+('seconds')
